7320e01b91f7c902c6ddba57cecc5f69046b763e
Volumio <info@volumio.org>
2020-04-16 16:23:10 +0000

    Version bump to 3.3.6

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 echo "Installing shairport-sync dependencies"
 apt-get update
-apt-get install -y build-essential git xmltoman autoconf automake libtool libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev checkinstall
+apt-get install -y build-essential git xmltoman autoconf automake libtool libdaemon-dev libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev checkinstall
 
 echo "Configuring shairport-sync"
 autoreconf -fi
-./configure --sysconfdir=/etc --with-alsa --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-dbus-interface
+./configure --sysconfdir=/etc --with-alsa --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-dbus-interface --with-metadata
 
 echo "Compiling"
 make
